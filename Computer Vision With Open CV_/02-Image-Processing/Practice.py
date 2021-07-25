6e73dcd7a400cb642d94e22260c7d18fb9d5318c
amanagrawal5510 <54680696+amanagrawal5510@users.noreply.github.com>
2021-07-25 17:04:13 +0530

    Add files via upload

---
+++ Computer Vision With Open CV_/02-Image-Processing/Practice.py
@@ -0,0 +1,24 @@
+#V1
+# =============================================================================
+# import numpy as np
+# import cv2
+# import matplotlib.pyplot as plt
+# 
+# img = cv2.imread('dog_backpack.jpg')
+# 
+# img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+# 
+# plt.imshow(img)
+# 
+# hsv = cv2.cvtColor(img,cv2.COLOR_RGB2HSV)
+# 
+# plt.imshow(hsv)
+# 
+# hls = cv2.cvtColor(img,cv2.COLOR_RGB2HLS)
+# 
+# plt.imshow(hls)
+# =============================================================================
+
+#V2
+
+
